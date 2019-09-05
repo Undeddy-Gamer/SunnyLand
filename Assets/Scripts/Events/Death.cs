5eb6eeee24d234fc2465d7c95b46bdbb455ae8fb
Undeddy-Gamer <ed.dixen@gmail.com>
2019-09-05 12:00:15 +1000

    added Spike

---
+++ Assets/Scripts/Events/Death.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+
+    public string hitTag = "Player";
+
+    public void ResetPlayer()
+    {
+        StartCoroutine(Die());
+
+        
+    }
+
+    IEnumerator Die()
+    {        
+        yield return new WaitForSeconds(0.4f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -57,4 +57,6 @@ public class GameManager : MonoBehaviour
         UIManager.Instance.UpdateScore(score);
     }
 
+
+
 }
